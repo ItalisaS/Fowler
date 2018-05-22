b856aa3a83d8fbec044b3c97196d4df7398c2419
z003nfpd <z003nfpd@MD1K33ZC.ad001.siemens.net>
2018-05-22 17:23:00 +0200

    Moved Amount Calculation to Rental-Class

--- scr/program/Customer.java
+++ scr/program/Customer.java
@@ -27,16 +27,16 @@ public class Customer {
 
 		while (enum_rentals.hasMoreElements()) {
 			double thisAmount = 0;
-			Rental rental = (Rental) enum_rentals.nextElement();
+			Rental each = (Rental) enum_rentals.nextElement();
 			// determine amounts for each line
-			thisAmount = amountFor(rental);
+			thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
+			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
@@ -44,26 +44,6 @@ public class Customer {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
-	}
-
-	private double amountFor(Rental rental) {
-		double thisAmount = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 
 }
